ba8b2c4f99b45e07163ebc051a1d2d72eb12dcec
Cristiano Araujo <cca2@cin.ufpe.br>
2019-10-26 12:48:35 -0300

    Removi arquivo ClassifyOrFinish

---
+++ StartseNewsClassifier/Support Classes/ActOnClassification.swift
@@ -0,0 +1,14 @@
+//
+//  ActOnCLassification.swift
+//  StartseNewsClassifier
+//
+//  Created by Cristiano Araújo on 19/10/19.
+//  Copyright © 2019 Cristiano Araújo. All rights reserved.
+//
+
+import SwiftUI
+
+protocol ActOnClassification {
+    func finishedClassification()
+    func restartClassification()
+}
