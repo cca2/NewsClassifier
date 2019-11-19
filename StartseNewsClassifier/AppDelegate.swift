2b59b5351b640362e480573946725483e2d9877a
Cristiano Araujo <cca2@cin.ufpe.br>
2019-11-19 15:20:41 -0300

    Criei tela inicial de registro de equipe

--- StartseNewsClassifier/AppDelegate.swift
+++ StartseNewsClassifier/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
         return true
     }
 
@@ -33,6 +42,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    //Support for Push Notification
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    // 1. Convert device token to string
+    let tokenParts = deviceToken.map { data -> String in
+    return String(format: "%02.2hhx", data)
+    }
+    let token = tokenParts.joined()
+    // 2. Print device token to use for PNs payloads
+    print("Device Token: \(token)")
+    let bundleID = Bundle.main.bundleIdentifier;
+        print("Bundle ID: \(token) \(String(describing: bundleID))");
+    // 3. Save the token to local storeage and post to app server to generate Push Notification. ...
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("failed to register for remote notifications: \(error.localizedDescription)")
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+    print("Received push notification: \(userInfo)")
+    let aps = userInfo["aps"] as! [String: Any]
+    print("\(aps)")
+    }
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentCloudKitContainer = {
